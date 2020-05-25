60c59dc333ba83c13a5dfe91c364edf09db9ae4b
Frank Kohlhepp <frank@kohlhepp.me>
2020-05-26 00:38:39 +0200

    Basic todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class ToDo(models.Model):
+    due_date = models.DateTimeField()
+    description = models.CharField(max_length=160)
+    percent = models.FloatField()
+    
+    def __str__(self):
+        return self.description
