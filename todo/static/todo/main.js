5ba7a3d6013587aa478995376b05089a2535cf2f
Frank Kohlhepp <frank@kohlhepp.me>
2020-05-27 22:09:39 +0200

    Now Button added

---
+++ todo/static/todo/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#button-now").click(function() {
+        var d = new Date();
+        $("#datetime").val(
+            d.getFullYear() + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" +
+            ("0" + d.getDate()).slice(-2) + "T" + ("0" + d.getHours()).slice(-2) +
+            ":" + ("0" + d.getMinutes()).slice(-2)
+        );
+    });
+});
