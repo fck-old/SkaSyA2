c485feb7100a92f5bbf4c9b15fcd1670bb39c8cf
Frank Kohlhepp <frank@kohlhepp.me>
2020-05-27 21:42:59 +0200

    error handling

--- todo/views.py
+++ todo/views.py
@@ -5,62 +5,67 @@ from django.utils import timezone
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-
-
 from .models import ToDo
 
-
-# Create your views here.
 class IndexView(generic.ListView):
     model = ToDo
     template_name = 'todo/index.html'
 
 def new(request):
-    if 'description' in request.POST:
-        
+    if request.method == 'POST':
         todo = ToDo()
         todo.due_date = request.POST['date']
         todo.description = request.POST['description']
         todo.percent = request.POST['percentdone']
-        todo.save()
-        
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('todo:index'))
-        
+        try:
+            if todo.description == '':
+                raise Exception('empty')
+            todo.save()
+        except:
+            context = {
+                'due_date': request.POST['date'],
+                'description': request.POST['description'],
+                'percent': request.POST['percentdone'],
+                'error': True
+            }
+            return render(request, 'todo/new.html', context)
+        else:
+            return HttpResponseRedirect(reverse('todo:index'))
     else:
         return render(request, 'todo/new.html')
 
 
 def edit(request, pk): 
     if request.method == 'POST':
-
-        todo = ToDo.objects.get(id=pk)
+        todo = get_object_or_404(ToDo, id=pk)
         todo.due_date = request.POST['date']
         todo.description = request.POST['description']
         todo.percent = request.POST['percentdone']
-        todo.save()
+        try:
+            if todo.description == '':
+                raise Exception('empty')
+            todo.save()
+        except:
+            context = {
+                'due_date': request.POST['date'],
+                'description': request.POST['description'],
+                'percent': request.POST['percentdone'],
+                'error': True
+            }
+            return render(request, 'todo/edit.html', context)
+        else:
+            return HttpResponseRedirect(reverse('todo:index'))
 
-        return HttpResponseRedirect(reverse('todo:index'))
-            
     else:  
-        todo = ToDo.objects.get(id=pk)
-        context = {
-            'due_date': todo.due_date,
-            'description':todo.description,
-            'percent':todo.percent, #or percentdone?!
-            'todo.id':pk
-        }  
-        return render(request, 'todo/edit.html', context) #HttpResponseRedirect(reverse('todo:edit')) #HttpResponse('could not find todo for this id') 
+        todo = get_object_or_404(ToDo, id=pk)
+        return render(request, 'todo/edit.html', todo.__dict__)
+
 
 def delete(request, pk):
-    todo = get_object_or_404(ToDo, id=pk)
-    todo.delete()
+    if request.method == 'POST':
+        todo = get_object_or_404(ToDo, id=pk)
+        todo.delete()
     return HttpResponseRedirect(reverse('todo:index'))
-
 
 def imprint(request):
     return render(request, 'todo/imprint.html')
-
-
