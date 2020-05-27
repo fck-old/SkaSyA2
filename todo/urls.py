8e584cbf36baae4410ff839da2c1d7e8a5e99331
Frank Kohlhepp <frank@kohlhepp.me>
2020-05-27 20:06:30 +0200

    bugfixes

--- todo/urls.py
+++ todo/urls.py
@@ -6,8 +6,7 @@ app_name = 'todo'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('new', views.new, name='new'),
-    path('edit/<int:pk>/', views.edit, name='edit'),
+    path('edit/<int:pk>', views.edit, name='edit'),
     path('delete/<int:pk>', views.delete, name='delete'),
-    #path('edit', views.edit, name='edit'),
     path('imprint', views.imprint, name='imprint'),
 ]
